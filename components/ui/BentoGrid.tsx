a817e69904c6c5b312abe38a2dd0d5ae72f61ac4
Ryan Lee <ryanbylee@ucla.edu>
2024-07-11 22:54:28 -0700

    fixed titles

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -45,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative rounded-3xl group/bento  shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
         className
       )}
       style={{
@@ -53,7 +53,7 @@ export const BentoGridItem = ({
         backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,151,255,1) 100%)',
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+      {/* <div className={`${id === 6} && 'flex justify-center h-full'`}>
         <div className="w-full h-full absolute">
           {img &&(
             <img
@@ -75,8 +75,8 @@ export const BentoGridItem = ({
             />
           )}
         </div>
-      </div>
-      <div className=" group-hover/bento:translate-x-2 transition duration-200">
+      </div> */}
+      <div className="">
         <div className=" font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
+      title: "단체사진",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-2 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
@@ -18,7 +18,7 @@ export const navItems = [
     },
     {
       id: 2,
-      title: "I'm very flexible with time zone communications",
+      title: "About BruinKSEA",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
@@ -28,8 +28,8 @@ export const navItems = [
     },
     {
       id: 3,
-      title: "My tech stack",
-      description: "I constantly try to improve",
+      title: "Events",
+      description: "Check out our upcoming events!",
       className: "lg:col-span-1 md:col-span-3 md:row-span-2",
       imgClassName: "",
       titleClassName: "justify-center",
@@ -38,8 +38,8 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Tech enthusiast with a passion for development.",
-      description: "",
+      title: "Calendar",
+      description: "Updated Weekly",
       className: "lg:col-span-1 md:col-span-3 md:row-span-1",
       imgClassName: "",
       titleClassName: "justify-start",
@@ -49,7 +49,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
+      title: "Featured",
       description: "The Inside Scoop",
       className: "md:col-span-2 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -59,7 +59,7 @@ export const navItems = [
     },
     {
       id: 6,
-      title: "Do you want to start a project together?",
+      title: "Contact Us",
       description: "",
       className: "lg:col-span-1 md:col-span-3 md:row-span-1",
       imgClassName: "",
